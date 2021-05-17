839a9412a447f5948d354ad40c385e58ab2fbe61
Andrea Edwards <aedwards@xula.edu>
2021-05-17 02:44:47 -0500

    first commit

---
+++ XUber.java
@@ -0,0 +1,24 @@
+/**
+ * XUber is a ridesharing app for the XULA community.
+ * The ridesharing vehicles are self-driving cars (max 4 occupants) and vans (max 8 occupants).
+ * 
+ * REGISTRATION
+ * Only once, users register using their first name and XULA email address and then confirm
+ * their registration by email and receive a unique rider key.
+ * 
+ * REQUEST A RIDE
+ * To request a ride, a user merely types their key and destination and then the nearest available
+ * vehicle is dispatched. Not every vehicle is available for every trip, instead the best vehicle is
+ * dispatched based on the user's destination, the vehicle location, and the number of occupants
+ * already in the vehicle.
+
+ * Every destination is at an intersection and has a destination number like in the map.txt file.
+ * 
+ */
+public class XUber {
+
+	public static void main(String[] args) {
+		System.out.println("\n\n==========  Welcome to XUber!  ==========\n");
+
+	}
+}
